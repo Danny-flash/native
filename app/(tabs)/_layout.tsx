19adbe9ab6ae7a9b0331f00c7d82afc14fc59e7a
unknown <daniel.funmiluyi@khaime.ai>
2025-06-24 05:20:02 +0100

    fix: some-issues

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,73 @@
+/* eslint-disable import/no-unresolved */
 import React from "react";
 import { Tabs } from "expo-router";
+import { ImageBackground, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
+const TabIcon = ({ focused, icon, title }: any) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={{
+          uri: "https://preview.redd.it/some-cute-purple-wallpapers-for-you-v0-3yeky2wvph7a1.jpg?width=620&format=pjpg&auto=webp&s=ae4069eeb98aa348dd13f7f9d0b92b785658fa54",
+        }}
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
+      >
+        {icon}
+        <Text className="text-secondary text-base font-semibold ml-2">
+          {title}
+        </Text>
+      </ImageBackground>
+    );
+  }
+  return (
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      {icon}
+    </View>
+  );
+};
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: '#0f0D23',
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: 'absolute',
+          overflow:'hidden',
+          borderWidth: 1,
+          borderColor: '#0f0D23'
+        }
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={
+                <Icon
+                  name="home"
+                  size={20}
+                  color={focused ? "#151312" : "#A8B5DB"}
+                />
+              }
+              title="Home"
+            />
+          ),
         }}
       />
       <Tabs.Screen
@@ -16,6 +75,19 @@ const _layout = () => {
         options={{
           title: "Search",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={
+                <Icon
+                  name="search"
+                  size={20}
+                  color={focused ? "#151312" : "#A8B5DB"}
+                />
+              }
+              title="Search"
+            />
+          ),
         }}
       />
       <Tabs.Screen
@@ -23,6 +95,19 @@ const _layout = () => {
         options={{
           title: "Saved",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={
+                <Icon
+                  name="save"
+                  size={20}
+                  color={focused ? "#151312" : "#A8B5DB"}
+                />
+              }
+              title="Saved"
+            />
+          ),
         }}
       />
       <Tabs.Screen
@@ -30,6 +115,19 @@ const _layout = () => {
         options={{
           title: "Profile",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={
+                <Icon
+                  name="verified-user"
+                  size={20}
+                  color={focused ? "#151312" : "#A8B5DB"}
+                />
+              }
+              title="Profile"
+            />
+          ),
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,25 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image, ScrollView } from "react-native";
+import React from "react";
 
 const Index = () => {
   return (
-    <SafeAreaView>
-      <Text>Index</Text>
-    </SafeAreaView>
-  )
-}
+    <View className="flex-1 bg-primary">
+      <Image
+        source={{
+          uri: "https://www.pixelstalk.net/wp-content/uploads/2016/05/Beautiful-Purple-Wallpaper.jpg",
+        }}
+        className="w-full h-full z-0 absolute"
+      />
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: '100%', paddingBottom: 10}}>
+        <Image
+          source={{
+            uri: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a58a7719-0dcf-4e0b-b7bb-d2b725dbbb8e/desyokd-fb6dd0d9-e0b1-48a5-a264-23545ca65605.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2E1OGE3NzE5LTBkY2YtNGUwYi1iN2JiLWQyYjcyNWRiYmI4ZVwvZGVzeW9rZC1mYjZkZDBkOS1lMGIxLTQ4YTUtYTI2NC0yMzU0NWNhNjU2MDUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.eOj81_VY2zp8snecg4zWVGVcnEftvM_mU53O4z8vzBY",
+          }}
+          className="w-20 h-20 mt-20 mx-auto mb-5"
+        />
+      </ScrollView>
+    </View>
+  );
+};
 
-export default Index
+export default Index;
